a89f914969583b471eae0edc507768e7edb5b552
horizoncarlo <carlo.gug@gmail.com>
2026-06-25 14:56:44 -0600

    PM2 script instead of screen

--- go-prod.sh
+++ go-prod.sh
@@ -1,2 +1,4 @@
-screen -d -m -S distantadventures sh -c "isProduction=true bun run start"
-screen -r
+#!/bin/bash
+#screen -d -m -S distantadventures sh -c "isProduction=true bun run start"
+#screen -r
+isProduction=true pm2 start bun --name "distantadventures" -- run start
